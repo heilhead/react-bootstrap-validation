4ba120e33908e73a4f3589527d9d7f418eabc107
Tobias Stevenson <codenamekt@gmail.com>
2016-05-19 08:48:25 -0500

    A couple tests.

--- __tests__/RenderTests.js
+++ __tests__/RenderTests.js
@@ -5,9 +5,10 @@ import Form from '../src/Form';
 describe('React bootstrap validation compilation test', () => {
     var React = require('react');
     var TestUtils = require('react-addons-test-utils');
+    var validator = require('validator');
 
     beforeEach(function() {
-        
+
     });
 
     it('Renders Form component correctly.', () => {
@@ -19,5 +20,22 @@ describe('React bootstrap validation compilation test', () => {
                   // Do some work with the validation outcomes
                 }
               } />);
-        });
+    });
+
+    it('Renders Form with validation library as prop.', () => {
+        // Assemble
+        var form = TestUtils.renderIntoDocument(
+            <Form validator={validator} onValidSubmit={ function(event) {return true;} } />
+         );
+    });
+
+    it('Form validation has custom validation rule.', () => {
+        // Assemble
+        var form = TestUtils.renderIntoDocument(
+            <Form validator={validator} onValidSubmit={ function(event) {return true;} } />
+        );
+         validator.extend('isTrue', val => { return true; });
+
+         expect(form.props.validator.isTrue()).toBe(true);
+    });
 });
